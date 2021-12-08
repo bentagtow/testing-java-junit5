ea6b5ba1cf6826ae858da113a65069dd9c7afc6f
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-08 11:23:55 -0600

    adds test for indexControlloer

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void index() {
+    }
+
+    @Test
+    void oupsHandler() {
+    }
+}
